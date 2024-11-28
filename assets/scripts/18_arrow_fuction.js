18021a7400a93c5f56a37dccedf723683bb40860
Oliveir4Matheus <contato.matheusoliv@outlook.com>
2024-11-28 13:42:05 -0300

    learn about arrow function

---
+++ assets/scripts/18_arrow_fuction.js
@@ -0,0 +1,10 @@
+// Arrow function - são funções anônimas que permitem escrever uma função com menos código sem perder a clareza do código.
+
+// Ele pode ser usada em contextos onde uma função realiza poucas instruções ou em contextos de callback.
+
+let menssage = (menssage) => {
+        new Error("adsfdasgf")
+        console.log(`-------------- ${menssage}-------------------`)
+}
+
+console.log(menssage("texto"))
